9e716118547991b9d93654d3d1b032aac78c15ed
Yasunori Mahata <mahata777@gmail.com>
2014-12-01 17:28:01 -0800

    Add Dummy API

--- web.py
+++ web.py
@@ -14,6 +14,13 @@ def index():
     return render_template("index.html", var={"hey": "yo"})
 
 
+@app.route("/dummy-api")
+def api():
+    response = app.make_response('{foo: "bar"}')
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
