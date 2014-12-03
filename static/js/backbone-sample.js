687ce06503b177b9fd5d3653702016e618057569
Yasunori Mahata <mahata777@gmail.com>
2014-12-03 15:06:43 -0800

    Working smallest example

--- static/js/backbone-sample.js
+++ static/js/backbone-sample.js
@@ -1,52 +1,38 @@
 $(function() {
     var Entry = Backbone.Model.extend({
-        defaults: function() {
-            title: "new entry title..."
-        }
+        defaults: function() {}
     });
 
     var EntryList = Backbone.Collection.extend({
         model: Entry
     });
 
-    // var Entries = new EntryList;
-
     var EntryView = Backbone.View.extend({
         tagName: "li",
         template: _.template($("#entry-template").html()),
 
-        events: {
-            "dbclick .entry-title": "dbclickDummy"
-        },
-
         initialize: function() {
-            // this.listenTo(this.model, "change", this.render);
+            this.model.bind("destroy", this.remove, this);
         },
 
         render: function() {
-            console.log("render method is called.");
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         },
-
-        dbclickDummy: function() {
-            console.log("dbclickDummy is called.");
-        }
     });
 
     var EntryListView = Backbone.View.extend({
         el: $("#entry-view"),
         events: {
-            "click #submit-button": "callApi"
+            "click #add-button": "callApi",
+            "click #remove-button": "removeEntries"
         },
 
         initialize: function() {
-            // this.listenTo(Entries, "add", this.add);
-            this.model.bind("add", this.render, this);
+             this.model.bind("add", this.render, this);
         },
 
         render: function() {
-            // console.log("render is called.");
             var entryListEl = $("#entry-list");
             entryListEl.empty();
 
@@ -57,22 +43,22 @@ $(function() {
             });
         },
 
-        // add: function(entry) {
-        //     console.log("add is called.");
-        // },
-
         callApi: function() {
             var self = this;
-            console.log("callApi is called.");
+
             $.ajax({
                 url: "/dummy-api"
             }).done(function(data) {
-                // console.log("ajax request done: ", self.model, data);
-                console.log(self.model);
-                self.model.add(new Entry({
-                    title: "Hello, new Entry"
-                }));
+                $.each(data["list-data"], function(idx, value) {
+                    self.model.add(new Entry({ title: value }));
+                });
             });
+
+            return false;
+        },
+
+        removeEntries: function() {
+            _.invoke(this.model.toArray(), "destroy");
 
             return false;
         }
